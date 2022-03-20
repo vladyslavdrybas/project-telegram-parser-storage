242f14b12f98495bc8444f2e5108161c73ade7f2
VladyslavDrybas <ukdvvw@gmail.com>
2022-03-20 19:41:58 +0200

    add find post

--- src/Controller/EvilChannelsController.php
+++ src/Controller/EvilChannelsController.php
@@ -20,7 +20,7 @@ class EvilChannelsController extends AbstractController
         $this->em = $entityManager;
     }
 
-    #[Route("/evil-channel", name: "contact_add", methods: ["POST"])]
+    #[Route("/evil-channel", name: "evil_channel_add", methods: ["POST"])]
     public function add(
         EvilChannelTransfer $transfer,
         EntityManagerInterface $entityManager
@@ -43,6 +43,31 @@ class EvilChannelsController extends AbstractController
             return new JsonResponse([
                 'success' => true,
                 'message' => 'new',
+            ]);
+        }
+
+        return new JsonResponse([
+            'success' => true,
+            'message' => 'exist',
+        ]);
+    }
+
+    #[Route("/evil-channel/{channel}/{id}", name: "check", methods: ["GET"])]
+    public function hasPost(
+        string $channel,
+        string $id,
+        EntityManagerInterface $entityManager
+    ) {
+        /** @var \App\Repository\EvilChannelRepository $repo */
+        $repo = $entityManager->getRepository(EvilChannel::class);
+        $existed = $repo->findOneBy([
+            'postId' => $channel . '/' . $id,
+        ]);
+
+        if (!$existed instanceof EvilChannel) {
+            return new JsonResponse([
+                'success' => false,
+                'message' => 'not exist',
             ]);
         }
 
