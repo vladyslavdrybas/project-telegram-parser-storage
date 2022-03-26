6b2037b8aa6d6342fac3a3e95c9a5435be3a21fe
VladyslavDrybas <ukdvvw@gmail.com>
2022-03-26 22:09:40 +0200

    fix migrations

--- migrations/Version20220324134200.php
+++ migrations/Version20220324134200.php
@@ -24,26 +24,31 @@ final class Version20220324134200 extends AbstractMigration
             !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\PostgreSQL100Platform,
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\PostgreSQL100Platform'."
         );
+        if (!$schema->hasTable('channel')) {
+            $this->addSql('CREATE TABLE channel (id UUID NOT NULL, title TEXT NOT NULL, main_link TEXT NOT NULL, message_link TEXT NOT NULL, active BOOLEAN DEFAULT \'true\' NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+            $this->addSql('CREATE UNIQUE INDEX uniq_a2f98e472b36786b ON channel (title)');
+            $this->addSql('COMMENT ON COLUMN channel.id IS \'(DC2Type:uuid)\'');
+            $this->abortIf(
+                !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\PostgreSQL100Platform,
+                "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\PostgreSQL100Platform'."
+            );
+        }
 
-        $this->addSql('CREATE TABLE channel (id UUID NOT NULL, title TEXT NOT NULL, main_link TEXT NOT NULL, message_link TEXT NOT NULL, active BOOLEAN DEFAULT \'true\' NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE UNIQUE INDEX uniq_a2f98e472b36786b ON channel (title)');
-        $this->addSql('COMMENT ON COLUMN channel.id IS \'(DC2Type:uuid)\'');
-        $this->abortIf(
-            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\PostgreSQL100Platform,
-            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\PostgreSQL100Platform'."
-        );
+        if (!$schema->hasTable('evil_channel')) {
+            $this->addSql('CREATE TABLE evil_channel (id UUID NOT NULL, platform VARCHAR(255) NOT NULL, link TEXT NOT NULL, post_id TEXT NOT NULL, meta TEXT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+            $this->addSql('CREATE UNIQUE INDEX uniq_f985a89e36ac99f14b89032c ON evil_channel (link, post_id)');
+            $this->addSql('COMMENT ON COLUMN evil_channel.id IS \'(DC2Type:uuid)\'');
+            $this->abortIf(
+                !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\PostgreSQL100Platform,
+                "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\PostgreSQL100Platform'."
+            );
+        }
 
-        $this->addSql('CREATE TABLE evil_channel (id UUID NOT NULL, platform VARCHAR(255) NOT NULL, link TEXT NOT NULL, post_id TEXT NOT NULL, meta TEXT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE UNIQUE INDEX uniq_f985a89e36ac99f14b89032c ON evil_channel (link, post_id)');
-        $this->addSql('COMMENT ON COLUMN evil_channel.id IS \'(DC2Type:uuid)\'');
-        $this->abortIf(
-            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\PostgreSQL100Platform,
-            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\PostgreSQL100Platform'."
-        );
-
-        $this->addSql('CREATE TABLE post (id UUID NOT NULL, channel TEXT NOT NULL, post_number INT NOT NULL, meta TEXT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE UNIQUE INDEX uniq_5a8a6c8da2f98e47a6b2fb31 ON post (channel, post_number)');
-        $this->addSql('COMMENT ON COLUMN post.id IS \'(DC2Type:uuid)\'');
+        if (!$schema->hasTable('post')) {
+            $this->addSql('CREATE TABLE post (id UUID NOT NULL, channel TEXT NOT NULL, post_number INT NOT NULL, meta TEXT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+            $this->addSql('CREATE UNIQUE INDEX uniq_5a8a6c8da2f98e47a6b2fb31 ON post (channel, post_number)');
+            $this->addSql('COMMENT ON COLUMN post.id IS \'(DC2Type:uuid)\'');
+        }
     }
 
     public function down(Schema $schema): void
